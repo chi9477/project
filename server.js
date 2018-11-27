8896b89fc4ab8e459f887ef0568fb9f34e587fc9
chi9477 <42923624+chi9477@users.noreply.github.com>
2018-11-27 12:08:09 +0800

    Update server.js

--- server.js
+++ server.js
@@ -6,9 +6,6 @@ var app = express();
 app = express();
 app.set('view engine','ejs');
 
-var SECRETKEY1 = 'I want to pass COMPS381F';
-var SECRETKEY2 = 'Keep this to yourself';
-
 var users = new Array(
 	{name: 'demo', password: ''},
 	{name: 'guest', password: 'guest'}
@@ -18,7 +15,6 @@ app.set('view engine','ejs');
 
 app.use(session({
   name: 'session',
-  keys: [SECRETKEY1,SECRETKEY2]
 }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
