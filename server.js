66d98efb47c0b6103016c004e241f324410a5471
chi9477 <42923624+chi9477@users.noreply.github.com>
2018-11-28 01:24:29 +0800

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ var assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
 var mongourl = 'mongodb://doublechi123:doublechi123@ds149682.mlab.com:49682/chi94';  // use your mlab database
 
+var products = [
+	{name: 'Apple iPad Pro', stock: 100, price: 7000, id:'001'},
+	{name: 'Apple iPhone 7', stock: 50, price: 7800, id:'002'},
+	{name: 'Apple Macbook', stock: 70, price: 11000, id: '003'}
+];
 
 app = express();
 app.set('view engine','ejs');
@@ -46,7 +51,7 @@ app.get('/read',function(req,res) {
 		res.redirect('/login');
 	} 
 	else {
-		res.render('restaurants',{name:req.session.username});										
+		res.render('restaurants',{name:req.session.username},{c: products});										
 	}
 });
 
