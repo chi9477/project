aaed50451d71b2e6478eea924148181ed21e13b3
chi9477 <42923624+chi9477@users.noreply.github.com>
2018-11-27 17:25:21 +0800

    Update server.js

--- server.js
+++ server.js
@@ -8,11 +8,9 @@ var ObjectId = require('mongodb').ObjectID;
 var url = 'mongodb://chi94:doublechi123@ds149682.mlab.com:49682/chi94';
 
 var insertDocument = function(db, callback) {
-   db.collection('books').insertOne( {
-	"name" : "Introduction to Node.js",
-	"author" : "John Dole",
-	"price" : 75.00,
-	"stock" : 0      
+   db.collection('restaurants').insertOne( {
+	   "_id":"2";
+	"name" : "Introduction to Node.js",  
    }, function(err, result) {
     assert.equal(err, null);
     console.log("Inserted a document into the books collection.");
