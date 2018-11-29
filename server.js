d4caaabe13a5cc414b0b23b0e2d13686c4b63de3
chi9477 <42923624+chi9477@users.noreply.github.com>
2018-11-30 02:19:24 +0800

    Update server.js

--- server.js
+++ server.js
@@ -290,6 +290,16 @@ app.post('/rate',function(req,res) {
 	MongoClient.connect(mongourl, function(err, db) {
 		assert.equal(err,null);
 			db.collection('restaurants').update({_id: ObjectId(req.body.id)}, {
+			    "name": req.body.name,
+			    "borough": req.body.borough,
+			    "cuisine": req.body.cuisine,
+			    "photo": "no.jpg",
+			    "photo mimetype": "KASDKJ",
+			    "street": req.body.street,
+			    "building": req.body.building,
+			    "zipcode": req.body.zipcode,
+			    "gps1": req.body.gps1,
+			    "gps2": req.body.gps2,
 			    "grades.user": req.session.username,     
 			    "grades.score": req.body.score	
 		});	
