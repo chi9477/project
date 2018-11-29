10ad7435ec89c6b3cd8d627259ff5b0633f307d1
chi9477 <42923624+chi9477@users.noreply.github.com>
2018-11-30 04:03:06 +0800

    Update server.js

--- server.js
+++ server.js
@@ -298,21 +298,10 @@ app.post('/rate',function(req,res) {
 		assert.equal(err,null);
 		if (req.session.username != req.body.user) {
 			db.collection('restaurants').update({_id: ObjectId(req.body.id)}, {
-				"name": req.body.name,
-			        "borough": req.body.borough,
-			    	"cuisine": req.body.cuisine,
-			    	"photo": "no.jpg",
-			    	"photo mimetype": "KASDKJ",
-			    	"street": req.body.street,
-			        "building": req.body.building,
-			    	"zipcode": req.body.zipcode,
-			   	"gps1": req.body.gps1,
-			    	"gps2": req.body.gps2,
-				"grades": {
-			    		"user": req.session.username,     
-			    		"score": req.body.score
-			    	},	
-			    	"owner": req.session.username
+				$set: {
+			    		"grades.user": req.session.username,     
+			    		"grades.score": req.body.score
+			    	}	
 		});
 			res.redirect('/');
 		} else {
