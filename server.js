4dac33c3bd940671384205a2b9a43318749d1e72
chi9477 <42923624+chi9477@users.noreply.github.com>
2018-11-29 20:27:17 +0800

    Update server.js

--- server.js
+++ server.js
@@ -250,14 +250,6 @@ app.post('/read',function(req,res) {
 		assert.equal(err,null);
         	db.collection("restaurants").find(
 			{name:req.body.search}
-			{borough:req.body.search}
-			{cuisine:req.body.search}
-			{street:req.body.search}
-			{building:req.body.search}
-			{zipcode:req.body.search}
-			{gps1:req.body.search}
-			{gps2:req.body.search}
-			{owner:req.body.search}
 			).toArray(function(err,items){
 		res.render('restaurants',{name:req.session.username, r:items});
 			});
