0e49f02c7bc9704616d49aeec3a6edc811e8e5fa
chi9477 <42923624+chi9477@users.noreply.github.com>
2018-11-29 20:34:36 +0800

    Update server.js

--- server.js
+++ server.js
@@ -249,8 +249,7 @@ app.post('/read',function(req,res) {
 		MongoClient.connect(mongourl, function(err, db) {
 		assert.equal(err,null);
         	db.collection("restaurants").find(
-			{name:req.body.search,owner:req.body.search}
-			).toArray(function(err,items){
+			{name,owner:req.body.search).toArray(function(err,items){
 		res.render('restaurants',{name:req.session.username, r:items});
 			});
         	});									
