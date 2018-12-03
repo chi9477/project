9abad30b453a04e8c454d16efa375b4d92960a47
chi9477 <42923624+chi9477@users.noreply.github.com>
2018-12-03 11:31:26 +0800

    Update server.js

--- server.js
+++ server.js
@@ -152,6 +152,16 @@ app.get('/showdetails', function(req,res) {
 				break;
 			}
 		}
+		if (!items[i].gps1) {
+			db.collection("grades").find({r_id: req.query.id}).toArray(function(err,rnames){
+					res.render('detailsnomap', {r: items[i], g: rnames});
+			});
+		}
+		if (!items[i].gps2) {
+			db.collection("grades").find({r_id: req.query.id}).toArray(function(err,rnames){
+					res.render('detailsnomap', {r: items[i], g: rnames});
+			});
+		}
 		if (item) {
 			db.collection("grades").find({r_id: req.query.id}).toArray(function(err,rnames){
 					res.render('details', {r: items[i], g: rnames});
