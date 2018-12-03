6d0e94431d1fbc0b559037e9ee4dd6cfcc9624e8
chi9477 <42923624+chi9477@users.noreply.github.com>
2018-12-03 11:59:52 +0800

    Update server.js

--- server.js
+++ server.js
@@ -152,19 +152,11 @@ app.get('/showdetails', function(req,res) {
 				break;
 			}
 		}
-		if (!items[i].photo) {
-			if (!items[i].gps1) {
-			db.collection("grades").find({r_id: req.query.id}).toArray(function(err,rnames){
-					res.render('detailsnophotonmap', {r: items[i], g: rnames});
-			});
-		}
-			if (!items[i].gps2) {
-			db.collection("grades").find({r_id: req.query.id}).toArray(function(err,rnames){
-					res.render('detailsnophotonmap', {r: items[i], g: rnames});
-			});
-		}
+		if (!items[i].photo) {			
 			
 			db.collection("grades").find({r_id: req.query.id}).toArray(function(err,rnames){
+				if (!items[i].gps1) {res.render('detailsnophotonmap', {r: items[i], g: rnames});}
+				if (!items[i].gps2) {res.render('detailsnophotonmap', {r: items[i], g: rnames});}
 					res.render('detailsnophoto', {r: items[i], g: rnames});
 			});
 		}
