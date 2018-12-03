6f56754b82b0ae7bf17aef24f1bd28c44d1297d2
chi9477 <42923624+chi9477@users.noreply.github.com>
2018-12-03 12:36:38 +0800

    Update server.js

--- server.js
+++ server.js
@@ -152,7 +152,7 @@ app.get('/showdetails', function(req,res) {
 				break;
 			}
 		}
-		if (!items[i].photo&&!items[i].gps1) {			
+		/*if (!items[i].photo&&!items[i].gps1) {			
 			
 			db.collection("grades").find({r_id: req.query.id}).toArray(function(err,rnames){
 				
@@ -168,7 +168,7 @@ app.get('/showdetails', function(req,res) {
 					res.render('detailsnophotonmap', {r: items[i], g: rnames});
 					
 			});
-		} 
+		} */
 		if (!items[i].photo) {	
 			
 			
@@ -178,7 +178,7 @@ app.get('/showdetails', function(req,res) {
 					
 			});
 		} 
-		if (!items[i].gps1) {
+		/*if (!items[i].gps1) {
 			db.collection("grades").find({r_id: req.query.id}).toArray(function(err,rnames){
 					res.render('detailsnomap', {r: items[i], g: rnames});
 			});
@@ -187,7 +187,7 @@ app.get('/showdetails', function(req,res) {
 			db.collection("grades").find({r_id: req.query.id}).toArray(function(err,rnames){
 					res.render('detailsnomap', {r: items[i], g: rnames});
 			});
-		}
+		}*/
 		if (item) {
 			db.collection("grades").find({r_id: req.query.id}).toArray(function(err,rnames){
 					res.render('details', {r: items[i], g: rnames});
@@ -443,10 +443,10 @@ app.get('/gps', function(req,res) {
 		if (item) {
 			res.render('gps', {r: items[i]});							
 		} else {
-			res.render('gps', {r: items[i]});							
+			res.status(500).end(req.query.id + ' not found!');
 		}
 	} else {
-			res.render('gps', {r: items[i]});							
+		res.status(500).end('id missing!');
 	}
 			});
 		});
