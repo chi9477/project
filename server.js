1f9ce6113119016a32418342db57a42810a247f3
chi9477 <42923624+chi9477@users.noreply.github.com>
2018-12-03 22:38:28 +0800

    Update server.js

--- server.js
+++ server.js
@@ -428,7 +428,7 @@ app.get('/gps', function(req,res) {
 app.get('/api/restaurant/:test1/Homantin',function(req,res){ 
 MongoClient.connect(mongourl, function(err, db) {
 	assert.equal(err,null);
-  db.collection("restaurants").find({req.params.test1: "Homantin"}).toArray(function(err,items){
+  db.collection("restaurants").find({req.query.test1: "Homantin"}).toArray(function(err,items){
 	res.status(200).json(items).end();
 	
 });
